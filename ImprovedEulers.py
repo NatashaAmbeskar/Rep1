04ed46e8bbc4d4f728612c2a3efead7effdb5ea5
NatashaAmbeskar <162603074+NatashaAmbeskar@users.noreply.github.com>
2024-05-22 10:40:22 -0400

    Update ImprovedEulers.py

--- ImprovedEulers.py
+++ ImprovedEulers.py
@@ -3,7 +3,7 @@ File Name: Improved Euler's Method
 Author: Natasha Ambeskar
 Date:3/6/2024
 
-Description:Calculates Improved Euler's Method given first order differential equation, initial values, increment size, and final x-value.
+Description:Performs Improved Euler's Method given first order differential equation, initial values, increment size, and final x-value.
 """
 from sympy import *
 from sympy import symbols
@@ -16,12 +16,12 @@ y0=1
 step=0.1
 xf=5
 
-def ImprovedEulers(diffEQ,x0,y0,xf,step):                  #uses Improved Euler's method to approximate value of function at xf
-    if(step>10**-6 and step<(xf-x0)):                      #prevents steps that are too small or to big(past the final value)
-        iterations=int((xf-x0)/step)                       #calculates the number of times Improved Euler's method must be performed to reach xf
-        for i in range(iterations):                        #Loop that performs the Improved Euler's method the number of times necessary to reach xF
-            m=diffEQ.subs(x,x0).subs(y,y0)
-            y0+=(step/2)*(m+diffEQ.subs(x,x0+step).subs(y,y0+m*step))
+def ImprovedEulers(diffEQ,x0,y0,xf,step):                              #Improved Euler's method to approximate value of function at xf
+    if(step>10**-6 and step<(xf-x0)):                                  #prevents steps that are too small or to big(past the final value)
+        iterations=int((xf-x0)/step)                                   #calculates the number of times Improved Euler's method must be performed to reach xf
+        for i in range(iterations):                                    #Loop that performs the Improved Euler's method the number of times necessary to reach xF
+            m=diffEQ.subs(x,x0).subs(y,y0)                 
+            y0+=(step/2)*(m+diffEQ.subs(x,x0+step).subs(y,y0+m*step))  #recalculates y using average of slope at initial values and values at the end of the step interval(Improved Euler's method)
             x0+=step
         return y0 
     else:
