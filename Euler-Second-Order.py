42a9087d34c66569fe80829dd4cda2db52bf99d7
NatashaAmbeskar <162603074+NatashaAmbeskar@users.noreply.github.com>
2024-05-28 21:10:18 -0400

    Create Euler-Second-Order.py

---
+++ Euler-Second-Order.py
@@ -0,0 +1,41 @@
+from sympy import *
+from math import *
+init_printing(use_unicode=True)
+x = Symbol("x")
+y = Symbol("y")
+yp = Symbol("yp") #represents y prime
+
+
+
+eqn2='-1*y+-x*yp' #Second order differential equation, can be changed by user.
+
+
+
+
+def EulersSecondOrder(eqn2,x0,y0,yp0,xf,step):#Second order Euler's method, accepts second order differential(eqn2) as a string along with x0,y0,yp0,xf,step size 
+    M=Matrix([[0,1]])
+    CoeffList=[]
+    for i in range(2):                        #Extracts coefficients from the equation and adds to a matrix
+        var=eqn2.find("y")
+        appending=sympify(eqn2[:var-1])
+        CoeffList.append(appending)
+        eqn2=eqn2[var+2:]
+    M=M.row_insert(1,Matrix([CoeffList]))
+
+    a=Matrix([y,yp])                         #Creates a new matrix to store y and y prime values
+
+    iterations=int((xf-x0)/step)
+    for i in range(iterations):             #Performs Euler's method on Matrix equation: a(next)=a(previous)+step*(M(with previous values subbed in)*a(previous)
+        f=M.subs({x:x0})*a.subs({y:y0,yp:yp0})
+        a=a.subs({y:y0,yp:yp0})+step*f
+        y0=a.row(0)[0]
+        yp0=a.row(1)[0]
+        x0+=step
+    return a                                 #returns a 2x1 matrix with the final y value in the first row and the final y prime value in the second row
+
+
+yfMatrix=EulersSecondOrder(eqn2,0,1,2,0.2,0.1) #parameters to be changed by user
+
+print('yf=',yfMatrix.row(0)[0])
+print('yfprime=',yfMatrix.row(1)[0])
+
